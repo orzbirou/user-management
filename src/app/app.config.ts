08ad722d3334ffeca8aae24366c60a25f3b2bd65
orzbirou <orzbirou@users.noreply.github.com>
2026-04-28 22:45:19 +0300

    feat: wire up ngrx store, effects, and devtools in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import * as userEffects from './features/users/store/user.effects';
+import { userReducer } from './features/users/store/user.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore({ users: userReducer }),
+    provideEffects(userEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
